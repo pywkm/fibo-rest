cd45ac72eb81ac634aae81f9692760b4ed9e47a1
Wiktor Matuszewski <pywkm@wukaem.pl>
2020-02-27 14:55:17 +0100

    Add working implementation fo generator

--- src/generator/fibonacci.py
+++ src/generator/fibonacci.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from time import sleep
 
 from config import DIFFICULTY, FIBO_QUEUE
+from messaging.abstract import Broker
 from messaging.broker import RabbitMqBroker
 
 
@@ -17,17 +18,19 @@ def callback(_ch, _method, _properties, body: bytes):
     calculate_up_to_index = message["length"] - 1
     first = FibonacciNum(*message["last_numbers"][0])
     second = FibonacciNum(*message["last_numbers"][1])
-    continue_sequence(calculate_up_to_index, first, second)
+    publish_sequence(calculate_up_to_index, first, second, RabbitMqBroker(), DIFFICULTY)
 
 
-def continue_sequence(up_to_index: int, first: FibonacciNum, second: FibonacciNum) -> None:
+def publish_sequence(
+    up_to_index: int, first: FibonacciNum, second: FibonacciNum, broker: Broker, difficulty: int
+) -> None:
     while second.index < up_to_index:
-        first, second = second, calculate_next_fibonacci(first, second, DIFFICULTY)
-        RabbitMqBroker().publish(FIBO_QUEUE, {second.index: second.value})
+        first, second = second, calculate_next_fibonacci(first, second, difficulty)
+        broker.publish(FIBO_QUEUE, {second.index: second.value})
 
 
 def calculate_next_fibonacci(
-    first: FibonacciNum, second: FibonacciNum, difficulty: int = 0
+    first: FibonacciNum, second: FibonacciNum, difficulty: int
 ) -> FibonacciNum:
-    sleep(difficulty / 1000)  # difficulty given in milliseconds
+    sleep(difficulty / 1000)  # difficulty given as timeout in milliseconds
     return FibonacciNum(second.index + 1, first.value + second.value)
---
+++ src/tests/test_generator.py
@@ -0,0 +1,41 @@
+from typing import Dict, List
+
+import mock
+import pytest
+
+from config import FIBO_QUEUE
+from generator.fibonacci import FibonacciNum, calculate_next_fibonacci, publish_sequence
+
+
+@pytest.mark.parametrize(
+    "first_fibo, second_fibo, expected_next_fibo",
+    [
+        (FibonacciNum(0, 0), FibonacciNum(1, 1), FibonacciNum(2, 1)),
+        (FibonacciNum(2, 1), FibonacciNum(3, 2), FibonacciNum(4, 3)),
+        (FibonacciNum(10, 55), FibonacciNum(11, 89), FibonacciNum(12, 144)),
+    ],
+)
+def test_calculating_next_fibonacci_numbers(
+    first_fibo: FibonacciNum, second_fibo: FibonacciNum, expected_next_fibo: FibonacciNum
+) -> None:
+    assert calculate_next_fibonacci(first_fibo, second_fibo, difficulty=0) == expected_next_fibo
+
+
+@pytest.mark.parametrize(
+    "first_fibo, second_fibo, up_to_index, expected_sequence",
+    [
+        (FibonacciNum(0, 0), FibonacciNum(1, 1), 3, [{2: 1}, {3: 2}]),
+        (FibonacciNum(2, 1), FibonacciNum(3, 2), 4, [{4: 3}]),
+        (FibonacciNum(10, 55), FibonacciNum(11, 89), 14, [{12: 144}, {13: 233}, {14: 377}]),
+    ],
+)
+def test_publishing_sequence(
+    broker_mock: mock.Mock,
+    first_fibo: FibonacciNum,
+    second_fibo: FibonacciNum,
+    up_to_index: int,
+    expected_sequence: List[Dict[int, int]],
+):
+    publish_sequence(up_to_index, first_fibo, second_fibo, broker_mock, difficulty=0)
+    expected_calls = [mock.call(FIBO_QUEUE, call) for call in expected_sequence]
+    broker_mock.publish.assert_has_calls(expected_calls)
